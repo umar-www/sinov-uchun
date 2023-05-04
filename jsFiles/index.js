98594f54f3b867837d9d3af1eb5b4d4caa7937ad
Umar-www <davronali84@gmail.com>
2023-05-04 14:17:26 +0000

    fead

---
+++ jsFiles/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Card = void 0;
+class Card {
+    constructor(firsName) {
+        this.firsName = firsName;
+    }
+}
+exports.Card = Card;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export class Card{
+    constructor(public firsName: string){
+        
+    }
+}
